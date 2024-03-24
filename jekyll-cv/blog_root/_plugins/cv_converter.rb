49ce703badcdd597bb2e97c904bf390dfde7c454
ryandaryl <ryandarylmills@gmail.com>
2024-03-24 17:59:12 +1100

    Fix left position of middle items in tilde list

--- jekyll-cv/blog_root/_plugins/cv_converter.rb
+++ jekyll-cv/blog_root/_plugins/cv_converter.rb
@@ -2,16 +2,21 @@ def format_tilde_lists(html)
   output = ''
   tilde_list_regex = /((?:.|\n)*?)(\n.+?)(\n +?~ .+?)(\n +?~ .+?)?(\n +?~ .+?)?\n((?:.|\n)*?)/
   html.scan(tilde_list_regex).each do |text_before, left_align, c1, c2, c3, text_after|
-    left_align, c1, c2, c3 = [left_align, c1, c2, c3].map{ |x| Kramdown::Document.new(
-      x.to_s.gsub(/\n +?~ +?/, '').delete("\n"), input: 'GFM'
-    ).to_html}
-    inline_list_template = <<~HTML
-      <div style=\"overflow: auto\">
-        <div style=\"float: left\">#{left_align}</div>
-        <div style=\"float: right\">#{c1}</div>
-      </div>\n
-    HTML
-    output << text_before + inline_list_template + text_after
+    list_items = [left_align, c1, c2, c3].compact
+    list_items = list_items.map do |x|
+      Kramdown::Document.new(
+        x.to_s.gsub(/\n +?~ +?/, '').delete("\n"), input: 'GFM'
+      ).to_html
+    end
+    attributes = list_items.each_with_index.map do |_x, i|
+      "position: absolute; left: #{
+        i * (100 / (list_items.length - 1))}%"
+    end
+    attributes[0] = 'float: left'
+    attributes[-1] = 'float: right'
+    div_list = list_items.zip(attributes).map { |item, attribute| "<div style=\"#{attribute}\">#{item}</div>" }
+    inline_list = "<div style=\"overflow: auto; position: relative;\">#{div_list.join('')}</div>\n"
+    output << text_before + inline_list + text_after
   end
   output
 end
