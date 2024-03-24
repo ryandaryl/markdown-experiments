d9fe7f5ce949c73b64e6cb6408bf49fda9bd9205
ryandaryl <ryandarylmills@gmail.com>
2024-03-24 17:20:51 +1100

    Move tilde list parsing to a separate method

--- jekyll-cv/blog_root/_plugins/cv_converter.rb
+++ jekyll-cv/blog_root/_plugins/cv_converter.rb
@@ -1,24 +1,30 @@
+def format_tilde_lists(html)
+  output = ''
+  tilde_list_regex = /((?:.|\n)*?)(\n.+?)(\n +?~ .+?)(\n +?~ .+?)?(\n +?~ .+?)?\n((?:.|\n)*?)/
+  html.scan(tilde_list_regex).each do |text_before, left_align, c1, c2, c3, text_after|
+    left_align, c1, c2, c3 = [left_align, c1, c2, c3].map{ |x| Kramdown::Document.new(
+      x.to_s.gsub(/\n +?~ +?/, '').delete("\n"), input: 'GFM'
+    ).to_html}
+    inline_list_template = <<~HTML
+      <div style=\"overflow: auto\">
+        <div style=\"float: left\">#{left_align}</div>
+        <div style=\"float: right\">#{c1}</div>
+      </div>\n
+    HTML
+    output << text_before + inline_list_template + text_after
+  end
+  output
+end
+
 class Jekyll::Converters::Markdown::CVProcessor
   def initialize(_config)
     super()
   end
 
   def convert(content)
-    output = ''
-    tilde_list_regex = /((?:.|\n)*?)(\n.+?)(\n +?~ .+?)(\n +?~ .+?)?(\n +?~ .+?)?\n((?:.|\n)*?)/
-    content.scan(tilde_list_regex).each do |text_before, left_align, c1, _, _, text_after|
-      left_align = Kramdown::Document.new(
-        left_align.delete("\n"), input: 'GFM'
-      ).to_html
-      output << text_before + <<~HTML
-        <div style=\"overflow: auto\">
-          <div style=\"float: left\">#{left_align}</div>
-          <div style=\"float: right\">#{c1.gsub(/\n +?~ +?/, '')}</div>
-        </div>\n#{text_after}
-      HTML
-    end
+    content = format_tilde_lists(content)
     Kramdown::Document.new(
-      output, input: 'GFM'
+      content, input: 'GFM'
     ).to_html
   end
 end
